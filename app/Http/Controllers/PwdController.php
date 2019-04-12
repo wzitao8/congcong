38a9bae6c00a7789034a58e0456c0028d855931b
admin <index@localhost.localdomain>
2019-04-12 07:15:32 +0000

    Pwduser

---
+++ app/Http/Controllers/PwdController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PwdController extends Controller
+{
+    public function user()
+    {
+        echo '123';
+    }
+}
